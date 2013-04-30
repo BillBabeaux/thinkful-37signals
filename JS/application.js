33110a10c3ac6f98f30c16aa1531b7e18374020b
Bill Babeaux <billbabeaux@gmail.com>
2013-04-29 23:14:07 -0400

    Add version of website with jQuery elements

---
+++ JS/application.js
@@ -0,0 +1,26 @@
+$(document).ready(function() {
+	$('#image-trigger-1').on('mouseenter', function(){
+		$('#replaced-section').html("<h1>Basecamp is the project management tool you wish you had on your last project.</h1><br><h2>Are you still managing projects with email? Are you still using Excel for your to-do lists? It's time to upgrade to Basecamp. Manage projects and collaborate with your team and clients the modern way.</h2>");
+	$('#image-trigger-1').on('mouseleave', function(){
+		$('#replaced-section').html("<h1>Making collaboration productive and enjoyable for people every day.</h1><br><h2>Frustration-free web-based apps for collaboration, sharing information, and making decisions.</h2>");
+	});
+	});
+});
+
+$(document).ready(function() {
+	$('#image-trigger-2').on('mouseenter', function(){
+		$('#replaced-section').html("<h1>Highrise remembers the important things about people you'd normally forget.</h1><br><h2>Keep a permanent record of the people you do business with. Know who you talked to, when you talked to them, what was said, and when to follow up next. Over 20,000,000 contacts are tracked using Highrise.</h2>");
+	$('#image-trigger-2').on('mouseleave', function(){
+		$('#replaced-section').html("<h1>Making collaboration productive and enjoyable for people every day.</h1><br><h2>Frustration-free web-based apps for collaboration, sharing information, and making decisions.</h2>");
+	});
+	});
+});
+
+$(document).ready(function() {
+	$('#image-trigger-3').on('mouseenter', function(){
+		$('#replaced-section').html("<h1>From near or far, Campfire helps teams work together over the web in real-time.</h1><br><h2>Share ideas, discussions, concepts, images, code samples, videos, documents, and mock-ups in real-time private room chat. It's game-changing. We couldn't run our business without Campfire.</h2>");
+	$('#image-trigger-3').on('mouseleave', function(){
+		$('#replaced-section').html("<h1>Making collaboration productive and enjoyable for people every day.</h1><br><h2>Frustration-free web-based apps for collaboration, sharing information, and making decisions.</h2>");
+	});
+	});
+});
